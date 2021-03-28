74fa23ff48c2eec3d64e7b0a879bb39809883daf
Sukhpreet Singh <66023067+sPreet247@users.noreply.github.com>
2021-03-28 18:02:00 -0400

    slimmingDownComponents

--- src/components/CardComponent/ImageComponent/ImageElements.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Images = styled.img`
-  width: 24.69rem;
-  height: 16.82rem;
-  border-radius: 1.5rem;
-  overflow: hidden;
-  margin-bottom: 0.938rem;
-`;
