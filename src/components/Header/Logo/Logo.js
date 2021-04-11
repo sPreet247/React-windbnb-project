d9c27140a3f125647e746ab40fabbce6f1cea735
Sukhpreet Singh <66023067+sPreet247@users.noreply.github.com>
2021-04-11 18:26:37 -0400

    refactor2

--- src/components/Header/Logo/Logo.js
+++ src/components/Header/Logo/Logo.js
@@ -1,7 +1,9 @@
+import logo from '../../../../assets/logo.svg';
+import { LogoImg } from './LogoElements';
 const Logo = () => {
   return (
     <>
-      <img src="/assets/triangleLogo.png" alt="Logo" />
+      <img src={logo} style={{ height: 100, width: 150 }} alt="Logo" />
     </>
   );
 };
