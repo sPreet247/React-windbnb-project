8ad644e28c081a34b7b212c6e3829589096d735e
Sukhpreet Singh <66023067+sPreet247@users.noreply.github.com>
2021-04-15 12:21:07 -0400

    guest2

--- src/components/Header/LocationDropdown/LocationDropdownElements.js
+++ src/components/Header/LocationDropdown/LocationDropdownElements.js
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components';
 export const Wrapper = styled.div`
   position: fixed;
   width: calc(50% - 11.5rem);
+
   top: 6rem;
   background: #fff;
   left: 8rem;
