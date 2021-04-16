d1e24b1d7e9688dbb15949ddac09e75ee91bc067
Sukhpreet Singh <66023067+sPreet247@users.noreply.github.com>
2021-04-16 11:06:44 -0400

    guest

--- src/components/Header/GuestDropdown/GuestDropdown.js
+++ src/components/Header/GuestDropdown/GuestDropdown.js
@@ -1,22 +1,11 @@
 import { UL, Wrapper } from './GuestDropdownElements';
 import {} from 'react-icons/md';
 function GuestDropdown() {
-  const Guest = [
-    'Helsinki, Finland',
-    'Turku, Finland',
-    'Vaasa, Finland',
-    'Oulu, Finland',
-  ];
+ 
   return (
     <>
       <Wrapper>
-        {Guest.map((item, index) => (
-          <div key={index}>
-            <UL>
-              <li>{item}</li>
-            </UL>
-          </div>
-        ))}
+        
       </Wrapper>
     </>
   );
