231260c7475eb13c02e8ad7821a49973e9eebdab
Preet Singh <66023067+sPreet247@users.noreply.github.com>
2021-05-02 15:51:53 -0400

    contextWorking

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './styles.css';
 import Card from './components/CardComponent/Main/Card';
 import Header from './components/Header/Main/Header';
-import AppState from './context/AppState'
 
 export default function App() {
   return (
     <>
-      <AppState>
-        <Header />
-        <Card />
-        <footer>created by Sukhpreet Singh - devChallenges.io</footer>
-      </AppState>
+      <Header />
+      <Card />
+      <footer>created by Sukhpreet Singh - devChallenges.io</footer>
     </>
   );
 }
