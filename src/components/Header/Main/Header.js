572deac84187ab6f462d8b24cdd41b8ede512fe2
Preet Singh <66023067+sPreet247@users.noreply.github.com>
2021-05-22 21:23:33 -0400

    wip3

--- src/components/Header/Main/Header.js
+++ src/components/Header/Main/Header.js
@@ -1,7 +1,7 @@
 import Logo from '../Logo/Logo';
 
-import NavSearch from '../NavSearch/NavSearch';
-import { Wrapper } from './HeaderElements';
+import NavSearch from '../NavSearch/NavSearch.js';
+import { Wrapper } from './HeaderElements.js';
 
 const Header = () => {
   return (
