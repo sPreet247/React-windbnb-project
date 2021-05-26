e503280ab467b4cfff2da05bd13073e3bd9a2552
Preet Singh <66023067+sPreet247@users.noreply.github.com>
2021-05-25 20:24:12 -0400

    wip2

--- src/components/Main/Header.js
+++ src/components/Main/Header.js
@@ -1,13 +1,15 @@
 import logo from '../../../../assets/logo.svg';
 import Description from '../Description/Description';
 import NavSearch from '../NavSearch/NavSearch';
-import { Wrapper } from './HeaderElements.js';
+import { Wrapper, NavWrap } from './HeaderElements.js';
 const Header = () => {
   return (
     <>
       <Wrapper>
-        <img src={logo} style={{ height: 100, width: 150 }} alt="Logo" />
-        <NavSearch />
+        <NavWrap>
+          <img src={logo} style={{ height: 100, width: 150 }} alt="Logo" />
+          <NavSearch />
+        </NavWrap>
         <Description />
       </Wrapper>
     </>
--- src/components/Main/HeaderElements.js
+++ src/components/Main/HeaderElements.js
@@ -4,3 +4,9 @@ export const Wrapper = styled.div`
   padding: 2rem 2rem;
   margin-bottom: 2rem;
 `;
+
+export const NavWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
