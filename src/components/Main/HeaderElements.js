c3565efdb7d40e168b10b604405958f3ee02a06f
Preet Singh <66023067+sPreet247@users.noreply.github.com>
2021-05-25 20:21:26 -0400

    wip

--- src/components/Main/HeaderElements.js
+++ src/components/Main/HeaderElements.js
@@ -1,9 +1,6 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
   padding: 2rem 2rem;
   margin-bottom: 2rem;
 `;
